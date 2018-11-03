fabfc5168d03a419be931123619d88430a2f5523
Manuel Astudillo <manuel@optimalbits.com>
2018-11-03 18:41:56 +0100

    prettier and added upgrade notice

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
 #! /usr/bin/env node
-const program = require('commander');
-const pkg = require('./package.json');
-const chalk = require('chalk');
+const program = require("commander");
+const pkg = require("./package.json");
+const chalk = require("chalk");
+
+// Check version
+const npmview = require("npmview");
+const semver = require("semver");
+
+// get local package name and version from package.json
+const pkgName = require("./package.json").name;
+const pkgVersion = require("./package.json").version;
 
 program
   .version(pkg.version)
-  .option('-n, --name [name]', 'connection name [My Connection]', 'My Connection')
-  .option('-t, --token [token]', 'api token (get yours at https://taskforce.sh)')
-  .option('-p, --port [port]', 'redis port [6379]', '6379')
-  .option('-h, --host [host]', 'redis host [localhost]', 'localhost')
-  .option('--passwd [passwd]', 'redis password')
-  .option('-b, --backend [host]', 'backend domain [api.taskforce.sh]', 'wss://api.taskforce.sh')  
+  .option(
+    "-n, --name [name]",
+    "connection name [My Connection]",
+    "My Connection"
+  )
+  .option(
+    "-t, --token [token]",
+    "api token (get yours at https://taskforce.sh)",
+    process.env.TASKFORCE_TOKEN
+  )
+  .option(
+    "-p, --port [port]",
+    "redis port [6379]",
+    process.env.REDIS_PORT || "6379"
+  )
+  .option(
+    "-h, --host [host]",
+    "redis host [localhost]",
+    process.env.REDIS_HOST || "localhost"
+  )
+  .option("--passwd [passwd]", "redis password", process.env.REDIS_PASSWD)
+  .option(
+    "-b, --backend [host]",
+    "backend domain [api.taskforce.sh]",
+    "wss://api.taskforce.sh"
+  )
   .parse(process.argv);
 
-console.info(chalk.blue('Taskforce Connector v' + pkg.version + ' - (c) 2017 Taskforce.sh Inc.'))
+console.info(
+  chalk.blue(
+    "Taskforce Connector v" + pkg.version + " - (c) 2017-2018 Taskforce.sh Inc."
+  )
+);
 
-const connection = {
-  port: program.port,
-  host: program.host,
-  password: program.passwd
-};
+npmview(pkgName, function(err, version, moduleInfo) {
+  if (semver.gt(version, pkgVersion)) {
+    chalk.red(
+      "New version " +
+        version +
+        " of taskforce available, please upgrade with yarn global add taskforce-connector"
+    );
+  } else {
+    const connection = {
+      port: program.port,
+      host: program.host,
+      password: program.passwd
+    };
 
-const socket = require('./lib/socket');
-socket(program.name, program.backend, program.token, connection);
+    const socket = require("./dist/socket");
+    socket(program.name, program.backend, program.token, connection);
+  }
+});
