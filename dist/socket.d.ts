09d24ee110ae74cad984414d8c011eec74637be9
Manuel Astudillo <manuel@optimalbits.com>
2021-06-29 18:01:49 +0200

    fix: remove socket.d.ts from dist

--- dist/socket.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Connection {
-    port?: number;
-    host?: string;
-    password?: string;
-    db?: number;
-    uri?: string;
-    tls?: object;
-}
-export declare const Socket: (name: string, server: string, token: string, connection: Connection, team?: string) => void;
